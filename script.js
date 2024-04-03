110e40dad3fd634bbe9b171a838d4cbaac0d10b0
Arthur Vinícius M <99282591+Arthurvdev@users.noreply.github.com>
2024-04-03 17:05:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function random(img){
+    document.querySelector(".img_gatinho").src = img
+}
+
+function link_img(dados) {
+    const img = dados[0].url
+    console.log(img)
+    random(img)
+}
+
+async function btn(){
+    const dados = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_5xYND8fOJuz5vPIl4tJKN05ABZbtWEWd035ehl7ufAczofRwawk3ktMvou1zIUmW`).then(response => response.json())
+    console.log(dados)
+    link_img(dados)
+}
